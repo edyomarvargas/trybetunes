ec0a04a01f97bb93e5d2501876fc49b992210013
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-07-11 18:57:57 -0300

    feat: alteracoes na imagem que aparece no header e no context api

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import AppContext from '../../context/AppContext';
 import logoWhite from '../../images/logo-white.svg';
 import userPhoto from '../../images/user.svg';
 import { SectionS1, SectionS2 } from './Style';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
 
 function Header() {
   const navigate = useNavigate();
@@ -15,15 +16,26 @@ function Header() {
       <SectionS1>
         <img src={ logoWhite } alt="logo-trybetunes" id="logo" />
 
-        <div>
-          <img src={ userPhoto } alt="user" id="user-photo" />
-          <p>{ user.name }</p>
-        </div>
+        <section>
+          <div>
+            <img
+              src={ user.image.length > 0 ? user.image : userPhoto }
+              alt="user"
+              id="user-photo"
+            />
+            <p>{ user.name }</p>
+          </div>
+          <RiLogoutBoxRLine
+            className="logout-btn"
+            size="36px"
+            onClick={ () => navigate('/')}
+          />
+        </section>
       </SectionS1>
 
       <SectionS2>
         <div
-          className={ pathname.includes('/search') ? 'selected' : 'not-selected' }
+          className={ pathname.includes('/search') || pathname.includes('/album') ? 'selected' : 'not-selected' }
           onClick={ () => navigate('/search') }
         >
           <p>Search</p>
